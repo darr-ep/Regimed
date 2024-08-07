cf5bb538677d07c05e509b682610035581d4db16
Th3Darrell <darr.pedraz@gmail.com>
2024-08-07 15:25:24 -0600

    Manejo de errores bd

--- config/database.js
+++ config/database.js
@@ -9,6 +9,15 @@ function obtenerConexion() {
   return new Promise((resolve, reject) => {
     pool.getConnection((err, connection) => {
       if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+          console.error('La conexión con la base de datos se cerró.');
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR') {
+          console.error('La base de datos tiene demasiadas conexiones.');
+        }
+        if (err.code === 'ECONNREFUSED') {
+          console.error('La conexión a la base de datos fue rechazada.');
+        }
         reject(err);
       } else {
         resolve(connection);
@@ -18,17 +27,22 @@ function obtenerConexion() {
 }
 
 async function ejecutarConsulta(query, params) {
-  const connection = await obtenerConexion();
-  return new Promise((resolve, reject) => {
-    connection.query(query, params, (err, results) => {
-      connection.release();
-      if (err) {
-        reject(err);
-      } else {
-        resolve(results);
-      }
+  try {
+    const connection = await obtenerConexion();
+    return new Promise((resolve, reject) => {
+      connection.query(query, params, (err, results) => {
+        connection.release();
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
     });
-  });
+  } catch (err) {
+    console.error('Error al obtener conexión:', err);
+    throw err;
+  }
 }
 
 module.exports = {
